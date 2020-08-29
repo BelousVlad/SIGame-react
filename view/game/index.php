781705e9ae378a5a8f9efb6fa13dc54e9269c104
ynoplanetashka1 <mr1ynoplanetashka@mail.ru>
2020-08-29 17:24:38 +0500

    added buttons to game page

--- view/game/index.php
+++ view/game/index.php
@@ -3,7 +3,8 @@ include ROOT.'\view\header.php';
 ?>
 <h1>GAME PAGE</h1>
 <a href = "../records">go to records page</a>
-
+<input type="button" name="create game" value="create game"/>
+<input type="button" name="getlobbylist" value="getlobbylist"/>
 <?
 include ROOT.'\view\footer.php';
 ?>
