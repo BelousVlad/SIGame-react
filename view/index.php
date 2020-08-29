cdde794924d3de8e7678fccde704021cbb5c66a2
ynoplanetashka1 <mr1ynoplanetashka@mail.ru>
2020-08-29 23:15:32 +0500

    link changes on main page

--- view/index.php
+++ view/index.php
@@ -4,13 +4,13 @@ include 'header.php';
 
 <div class="container">
   <div class="main-menu-container">
-    <a href="game/index.php" class="main-menu-btn main-menu-play-btn">
+    <a href="game" class="main-menu-btn main-menu-play-btn">
         Play
     </a>
-     <a href="game/index.php" class="main-menu-btn main-menu-play-btn">
+     <a href="record" class="main-menu-btn main-menu-play-btn">
         Records
     </a>
-     <a href="records/index.php" class="main-menu-btn main-menu-play-btn">
+     <a href="game/index.php" class="main-menu-btn main-menu-play-btn">
         Help
     </a>
      <a href="game/index.php" class="main-menu-btn main-menu-play-btn">
--- view/records/index.php
+++ view/records/index.php
@@ -4,6 +4,6 @@ include ROOT.'\view\header.php';
 
 <h1> RECORDS PAGE <? echo $name; ?></h1>
 
-      <a href="../index.php">go to main page</a>
+      <a href="..">go to main page</a>
 <?
 include ROOT.'\view\footer.php';?>
