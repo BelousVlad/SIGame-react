c865367d14f12a1e8013efd2aed0170abc0c41a7
ynoplanetashka1 <mr1ynoplanetashka@mail.ru>
2020-08-30 16:03:22 +0500

    craete CreateLobby.js

---
+++ js/CreateLobby.js
@@ -0,0 +1,31 @@
+// if ( typeof (document.getElementsByName('send')[0]) !== 'undefined')
+
+// document.getElementsByName('send')[0].onclick = () =>{
+
+// 	console.log("start of request");
+
+// 	let message = "&title=" + document.querySelector('#title').value + "&password=123&max_size=5&path=gg.pdf";
+
+// 	(new XMLRequest("../../index.php", "CreateLobby=1" + message)).then(
+
+// 	(res) => {
+// 		console.log(res);
+// 	}
+// 	)
+	// }
+	//
+	//
+	//
+	CreateLobby = () => {
+
+	console.log("start of request");
+
+	let message = "&title=" + document.querySelector('#title').value + "&password=123&max_size=5&path=gg.pdf";
+
+	(new XMLRequest("../../index.php", "CreateLobby=1" + message)).then(
+
+	(res) => {
+		console.log(res);
+	}
+	)
+	}
--- view/game/create.php
+++ view/game/create.php
@@ -14,25 +14,13 @@
 <input type="input" name="max_size" value="5" id="max_size"/>
 <input type="input" name="rules" value="rules" id="rules"/>
 <input type="input" name="pack" value="pack" id="path"/>
-<input type="button" name="send" value="send" id="title"/>
+<input type="button" name="send" value="send" id="title" onclick="CreateLobby();"/>
 
 
 
 
 <script type="text/javascript">
-	document.getElementsByName('send')[0].onclick = () =>{
 
-	console.log("start of request");
-
-	let message = "&title=" + document.querySelector('#title').value + "&password=123&max_size=5&path=gg.pdf";
-
-	(new XMLRequest("../../index.php", "CreateLobby=1" + message)).then(
-
-	(res) => {
-		console.log(res);
-	}
-	)
-	}
 
 
 
