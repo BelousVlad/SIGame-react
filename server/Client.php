4ab6bc54dcc0e10981dcb1b0ca1234216f11ee8b
ynoplanetashka1 <mr1ynoplanetashka@mail.ru>
2020-09-16 20:16:41 +0500

    added field server to Client class

--- server/Client.php
+++ server/Client.php
@@ -4,10 +4,12 @@ class Client {
 	public $lobby;
 	public $connection;
 	public $answerer;
+	public $server;
 
 	public function __construct($connection, $server)
 	{
 		$this->connection = $connection;
+		$this->server = $server;
 		$this->answerer = new Answerer($this, $server);
 
 // TODO move var server to Client ;
