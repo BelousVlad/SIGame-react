0afb7dd83b613bc3175de0037e4504562c3e1520
ynoplanetashka1 <mr1ynoplanetashka@mail.ru>
2021-01-27 14:33:51 +0500

    Game class

---
+++ server/socket-server/Game.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const fs = require('fs'),
+	  parseStringToXML = require('xml2js').parseString
+
+class Question{
+	constructor( theme, index ){
+		this.theme = theme;
+		this.index = index;
+	}
+}
+
+class Game{
+	constructor(lobby){
+		this.lobby = lobby;
+		this.filePath = lobby.filePath;
+		this.rules = lobby.rules;
+		var defaultRules = {
+			answerTimeAwait : 2e3 /* 2sec */,
+			//
+		}
+		for ( let i in defaultRules )
+			this.rules[i] = typeof this.rules[i] === 'undefined' ? defaultRules[i] : this.rules[i];
+
+		this.current = {
+			round : 0,
+			question : undefined
+		}
+		parseStringToXML(fs.readFileSync( this.filePath ), function( err, json ) {
+			this.package = json.package;
+			this.rounds = this.package.rounds;
+		}.bind(this));
+	}
+
+	checkQuestion( question ) {
+		question = this.getQuestion( question );
+		if ( !question )
+			return;
+		this.current.question = question;
+		question.checked = true;
+		this.update();
+	}
+
+	selectRound( roundIndex ) {
+		this.current.round = roundIndex;
+		this.update();
+	}
+
+	reloadCurrentRound() {
+		this.current.round.themes.forEach( item => {
+			item.questions.forEach( item_ => {
+				item_.checked = false;
+			} )
+		})
+	}
+
+	hasQuestion( question ) {
+		return !!this.getQuestion( question );
+	}
+
+	getQuestion( question ){
+		let theme = this.current.round.themes.find( item => {
+			return item.$.name === question.theme;
+		} )
+		return theme.questions[ question.index ];
+	}
+
+	update() {
+		// update users view
+	}
+}
+
+module.exports = Game;
